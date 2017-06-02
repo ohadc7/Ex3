d458aebb5a5727396fa6909f0b28783ed2176a7b
ido <idoklein1@gmail.com>
2017-06-02 17:50:19 +0300

    not interesting

---
+++ jquery.maze.js
@@ -0,0 +1,11 @@
+/*
+// JavaScript source code
+        var canvas = document.getElementById("myCanvas");
+        for (var i = 0; i<10; i++) {
+            for (var j = 0; j < 10; j++) {
+                var ctx = canvas.getContext("2d");
+                ctx.strokeStyle = "red";
+                ctx.strokeRect(i * canvas.width / 10, j * canvas.height / 10, canvas.width / 10, canvas.height / 10);
+            }
+        }
+		*/
