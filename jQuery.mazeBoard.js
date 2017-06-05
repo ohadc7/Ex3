de753f7af25d682dcf7eefe0fd95f921ebfd9adf
ido <idoklein1@gmail.com>
2017-06-05 13:52:18 +0300

    mazeBoard plugin

---
+++ jQuery.mazeBoard.js
@@ -0,0 +1,140 @@
+/*
+* credit: thanks to the first answer in
+* https://stackoverflow.com/questions/1117086/how-to-create-a-jquery-plugin-with-methods/22976877#22976877
+*/
+
+(function( $ ){
+
+    var methods = {
+        mazeBoard : function(mazeData, startRow, startCol, exitRow, exitCol, playerImage, exitImage //rows, cols, initialPos, goalPos
+																									) {
+			$(this).data("mazeData", mazeData);
+			$(this).data("startPos", {row: startRow, col: startCol});
+			$(this).data("playerPos", {row: startRow, col: startCol});
+			$(this).data("exitPos", {row: exitRow, col: exitCol});
+			$(this).data("playerImage", playerImage);
+			$(this).data("exitImage", exitImage);		
+        },
+
+        drawMaze : function( ) {
+			var thisCanvas = $(this)[0];
+			var userImg = $(this).data("playerImage");
+			var endImg = $(this).data("exitImage");
+			var maze = $(this).data("mazeData");
+			var myCanvas = $(this);
+			var context = thisCanvas.getContext("2d");
+			var rows = maze.length;
+			var cols = maze[0].length;
+			var cellWidth = thisCanvas.width / cols;
+			var cellHeight = thisCanvas.height / rows;
+			
+			for (var i = 0; i < rows; i++) {
+				for (var j = 0; j < cols; j++) {
+					if (maze[i][j] == 1) {
+						context.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
+					}
+				}
+			}
+			const goalPosRow = $(this).data("exitPos").row;
+			const goalPosCol = $(this).data("exitPos").col;
+			const currPosRow = $(this).data("playerPos").row;
+			const currPosCol = $(this).data("playerPos").col;
+			
+			window.onload = function() {
+				context.drawImage(userImg, currPosCol * cellWidth, currPosRow * cellHeight, cellWidth, cellHeight);
+        		context.drawImage(endImg, goalPosCol * cellWidth, goalPosRow * cellHeight, cellWidth, cellHeight);
+			}
+		},
+
+        hide : function( ) {  },
+
+        update : function( content ) {  }
+
+    };
+
+    $.fn.mazeBoard = function(methodOrOptions) {
+        if ( methods[methodOrOptions] ) {
+            return methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
+            // Default to "init"
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.mazeBoard' );
+        }    
+    };
+
+})( jQuery );
+
+
+/*
+// JavaScript source code
+(function ($) {
+	$.fn.mazeBoard = function (
+	//canvas, 
+	maze, rows, cols, initialPos, goalPos) {
+
+		//_canvas = document.getElementById(canvas);
+		_canvas = $(this);
+		_maze = maze;
+		_rows = rows;
+		_cols = cols;
+		_initialPos = initialPos;
+		_goalPos = goalPos;
+		_currPos = initialPos;
+
+
+		//drawMaze = function () {
+
+			var userImg = document.getElementById("user");
+			var endImg = document.getElementById("end");
+			var maze = this._maze;
+			var myCanvas = _canvas;
+			
+			//var currWidth = $(this).width();
+			
+			var context = this.getContext("2d");
+			var rows = maze.length;
+			var cols = maze[0].length;
+			var cellWidth = this._canvas.width / cols;
+			var cellHeight = this._canvas.height / rows;
+
+			for (var i = 0; i < rows; i++) {
+				for (var j = 0; j < cols; j++) {
+					if (maze[i][j] == 1) {
+						context.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
+					}
+				}
+			}
+			const goalPosRow = this._goalPos.row;
+			const goalPosCol = this._goalPos.col;
+			const currPosRow = this._currPos.row;
+			const currPosCol = this._currPos.col;
+			window.onload = function() {
+				context.drawImage(userImg, currPosCol * cellWidth, currPosRow * cellHeight, cellWidth, cellHeight);
+        		context.drawImage(endImg, goalPosCol * cellWidth, goalPosRow * cellHeight, cellWidth, cellHeight);
+			}
+		
+		//};
+
+		// Implement your plugin here
+		//var x = 0;
+		//alert(x);
+
+		return this;
+	};
+})(jQuery);
+
+
+class Position {
+	constructor(row, col) {
+		this._row = row;
+		this._col = col;
+	}
+	get row() {
+		return this._row;
+	}
+	get col() {
+		return this._col;
+	}
+}
+*/
