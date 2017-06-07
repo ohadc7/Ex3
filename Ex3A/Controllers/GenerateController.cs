dbbe0cab99b84e75b279d6a58a2560a87e79ea13
ohad cohen <ohadc7@gmail.com>
2017-06-08 00:21:03 +0300

    add post script to get maze

---
+++ Ex3A/Controllers/GenerateController.cs
@@ -0,0 +1,37 @@
+﻿using MazeGeneratorLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Ex3A.Controllers
+{
+    public class GenerateController : ApiController
+    {
+        /* private static List<Product> products = new List<Product> {
+             new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+             new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+         };*/
+        /*  public IEnumerable<Product> GetAllProducts()
+          {
+              return products;
+          }*/
+      
+        public IHttpActionResult GetGenerateMaze(string name, int rows, int cols)
+        {
+            var dfsMazeGenerator = new DFSMazeGenerator();
+            var MyMaze = dfsMazeGenerator.Generate(rows, cols);
+            MyMaze.Name = name;
+           // DictionaryOfMazes[name] = MyMaze;
+            return Ok(MyMaze);
+        }
+        /*[HttpPost]
+        public void AddProduct(Product p)
+        {
+            products.Add(p);
+        }*/
+    }
+}
