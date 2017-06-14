55ccef43b3f2bd5cdac02f85c3a437d0eaa5585c
ohad cohen <ohadc7@gmail.com>
2017-06-14 15:25:04 +0300

    middle of register

--- Ex3A/Scripts/registerScript.js
+++ Ex3A/Scripts/registerScript.js
@@ -1,4 +1,23 @@
-﻿$("#btnRegister").click(function(){
-    var usr = $("#userName").val();
-   // $.getJason
-});
+﻿var ViewModel = function () {
+    var self = this; // make 'this' available to subfunctions or closures
+    self.users = ko.observableArray(); // enables data binding
+    self.addUser = function () {
+        var usersUri = '/api/Users/';
+        var user = {
+            Name: self.newUser.Username(),
+            Password: self.newUser.Password(),
+            Email: self.newUser.Email()
+        };
+
+        $.getJSON(usersUri).done(function (data) {
+            console.log(data);
+        });
+
+        $.post(usersUri, user).done(function (item) {
+            self.users.push(item);
+        });
+    }
+};
+ko.applyBindings(new ViewModel());
+
+
