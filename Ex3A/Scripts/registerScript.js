6f21d9bc2371b33d8267c9dc639cdb12d9956ffc
ohad cohen <ohadc7@gmail.com>
2017-06-15 03:03:37 +0300

    rePassword check - finish encrypt password in register - finish cosmetics.

--- Ex3A/Scripts/registerScript.js
+++ Ex3A/Scripts/registerScript.js
@@ -1,15 +1,30 @@
-﻿var ViewModel = function () {
+﻿window.onload = function () {
+    document.getElementById("rePassword").onchange = checkPassword;
+}
+
+function checkPassword() {
+    var password = document.getElementById("password").value;
+    var rePassword = document.getElementById("rePassword").value;
+    if (password != rePassword) {
+        new PNotify({
+            title: 'Password Error!',
+            text: 'The Password must be match!',
+        });
+    }
+}
+
+var ViewModel = function () {
     var self = this; // make 'this' available to subfunctions or closures
     self.users = ko.observableArray(); // enables data binding
     self.Username = ko.observable();
     self.Password = ko.observable();
     self.Email = ko.observable();
-    self.RePassword = ko.observable();
     self.addUser = function () {
         var usersUri = '/api/Users/';
+        var encryptPassword = b64_sha512(self.Password());
         var user = {
             Name: self.Username(),
-            Password: self.Password(),
+            Password: encryptPassword,
             Email: self.Email()
         };
 
