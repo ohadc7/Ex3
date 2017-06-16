cb6652482933a829db8b8a5208d76e20fffa18a5
ohad cohen <ohadc7@gmail.com>
2017-06-16 13:41:44 +0300

    change nav after sign in/register  - finish

--- Ex3A/Scripts/navBar.js
+++ Ex3A/Scripts/navBar.js
@@ -1,6 +1,15 @@
-﻿$(document).ready(function () {
-    $("#navBar").load("NavBar.html");
+﻿$("#navBar").load("NavBar.html", function () {
     if (sessionStorage.getItem("userName")) {
-        
+        document.getElementById("registerId").textContent = "Hello " + sessionStorage.getItem("userName");
+        document.getElementById("registerId").href = "#";
+        document.getElementById("loginId").textContent = "Log Off";
+        document.getElementById("loginId").onclick = logOff;
+        document.getElementById("loginId").href = "HomePage.html";
+
     }
-})
+});
+
+function logOff() {
+    
+    sessionStorage.removeItem("userName");
+};
